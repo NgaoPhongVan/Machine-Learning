0921fbc853c903c7527719c566f48827c9d8e236
NgaoPhongVan <117790567+NgaoPhongVan@users.noreply.github.com>
2023-11-06 16:53:36 +0700

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import nltk
 nltk.download('wordnet')
-# read the file
+
 reviews_train = []
 for line in open('full_train.txt', 'r', encoding='utf-8'):
     reviews_train.append(line.strip())
@@ -10,7 +10,6 @@ reviews_test = []
 for line in open('full_test.txt', 'r', encoding='utf-8'):
     reviews_test.append(line.strip())
 
-# clean and preprocess
 import re
 REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\\[\\]]")
 REPLACE_WITH_SPACE = re.compile("(<br\\s*/><br\\s*/>)|(\\-)|(\\/)")
@@ -23,7 +22,6 @@ def preprocess_reviews(reviews):
 reviews_train_clean = preprocess_reviews(reviews_train)
 reviews_test_clean = preprocess_reviews(reviews_test)
 
-# nomorlization
 def get_lemmatized_text(corpus):
     from nltk.stem import WordNetLemmatizer
     lemmatizer = WordNetLemmatizer()
@@ -32,7 +30,6 @@ def get_lemmatized_text(corpus):
 reviews_train_clean = get_lemmatized_text(reviews_train_clean)
 reviews_test_clean = get_lemmatized_text(reviews_test_clean)
 
-# vectorization
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 stop_words = ['in', 'of', 'at', 'a', 'the']
@@ -41,7 +38,6 @@ tfidf_vectorizer.fit(reviews_train_clean)
 X = tfidf_vectorizer.transform(reviews_train_clean)
 X_test = tfidf_vectorizer.transform(reviews_test_clean)
 
-# classifier find c
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
@@ -52,7 +48,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 target = [1 if i < 12500 else 0 for i in range(25000)]
-# split the train set into training data and validation data
 X_train, X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
 
 # accuracy = []
@@ -61,15 +56,14 @@ X_train, X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75)
 #     lr = LinearSVC(C = c)
 #     lr.fit(X_train, y_train)
 #     accuracy.append(accuracy_score(y_val, lr.predict(X_val)))
-#     print("Accuracy for C = %s: %s" % (c, accuracy_score(y_val, lr.predict(X_val))))
+#     print("Độ chính xác với C = %s: %s" % (c, accuracy_score(y_val, lr.predict(X_val))))
 
 
-# train final model with highest c
 # highestC = [regularization[i] for i in range(len(regularization)) if accuracy[i] == max(accuracy)][0]
 final_model = LinearSVC(C = 2, dual = True, max_iter = 1000)
 final_model.fit(X, target)
 y_pred = final_model.predict(X_test)
-print("Final Accuracy: %s" % accuracy_score(target, y_pred))
+print("Độ chính xác cuối: %s" % accuracy_score(target, y_pred))
 # print(str(highestC))
 # accuracy.append(accuracy_score(target, final_model.predict(X_test)))
 # regularization.append('testError C:' + 1)
