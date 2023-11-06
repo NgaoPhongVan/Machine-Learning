bad6f6f040e4bcd64b59d3199abdb21e69629638
NgaoPhongVan <117790567+NgaoPhongVan@users.noreply.github.com>
2023-11-06 16:49:55 +0700

    Update Logistic.py

--- Logistic.py
+++ Logistic.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import nltk
 nltk.download('wordnet')
-# read the file
+
 reviews_train = []
 for line in open('full_train.txt', 'r', encoding='utf-8'):
     reviews_train.append(line.strip())
@@ -10,7 +10,7 @@ reviews_test = []
 for line in open('full_test.txt', 'r', encoding='utf-8'):
     reviews_test.append(line.strip())
 
-# clean and preprocess
+
 import re
 REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\\[\\]]")
 REPLACE_WITH_SPACE = re.compile("(<br\\s*/><br\\s*/>)|(\\-)|(\\/)")
@@ -24,7 +24,6 @@ def preprocess_reviews(reviews):
 reviews_train_clean = preprocess_reviews(reviews_train)
 reviews_test_clean = preprocess_reviews(reviews_test)
 
-# nomorlization
 def get_lemmatized_text(corpus):
     from nltk.stem import WordNetLemmatizer
     lemmatizer = WordNetLemmatizer()
@@ -33,7 +32,6 @@ def get_lemmatized_text(corpus):
 reviews_train_clean = get_lemmatized_text(reviews_train_clean)
 reviews_test_clean = get_lemmatized_text(reviews_test_clean)
 
-# vectorization
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 stop_words = ['in', 'of', 'at', 'a', 'the']
@@ -43,7 +41,6 @@ cv.fit(reviews_train_clean)
 X = cv.transform(reviews_train_clean)
 X_test = cv.transform(reviews_test_clean)
 
-# classifier find c
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
@@ -54,7 +51,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 target = [1 if i < 12500 else 0 for i in range(25000)]
-# split the train set into training data and validation data
 X_train, X_val, y_train, y_val = train_test_split(X, target, train_size = 0.75 )
 
 accuracy = []
@@ -65,7 +61,6 @@ for c in regularization:
     accuracy.append(accuracy_score(y_val, lr.predict(X_val)))
     print("Độ chính xác với C = %s: %s" % (c, accuracy_score(y_val, lr.predict(X_val))))
 
-# train final model with highest c
 highestC = [regularization[i] for i in range(len(regularization)) if accuracy[i] == max(accuracy)][0]
 final_model = LogisticRegression(C = highestC)
 final_model.fit(X, target)
@@ -80,7 +75,6 @@ from sklearn.metrics import confusion_matrix
 tn, fp, fn, tp = confusion_matrix(y_pred, target).ravel()
 print(str((tn, fp, fn, tp)))
 
-# top positive negative word
 # feature_to_coef = {
 #     word: coef for word, coef in zip(
 #         cv.get_feature_names_out(), final_model.coef_[0]
